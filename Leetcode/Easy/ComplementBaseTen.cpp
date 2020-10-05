8c9a56917504bbcea98367a388450b7395d3bb2a
Karan Hejmadi <hejmadikaran9@gmail.com>
2020-10-05 23:21:29 +0530

    day 10 problems

--- Leetcode/Easy/ComplementBaseTen.cpp
+++ Leetcode/Easy/ComplementBaseTen.cpp
@@ -1,24 +1,29 @@
-#include<bits/std++.h>
-
+#include <bits/stdc++.h>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    int bitwiseComplement(int N) {
-	    if(N==0) return 1;
-	    int k;
-	    for(int i=0;i<N;i++) {
-		    if(N>pow(2,i)-1) {
-			    k = pow(2,i)-1;
-			    break;
-		    }
-	    }
-	return k-N;		    
-	    
-    }
-}if(N<2) return !N;
-	    int k;
-        int i = 0;
+	int bitwiseComplement(int N)
+	{
+		if (N == 0)
+			return 1;
+		int k;
+		for (int i = 0; i < N; i++)
+		{
+			if (N > pow(2, i) - 1)
+			{
+				k = pow(2, i) - 1;
+				break;
+			}
+		}
+		return k - N;
+	}
+} if (N < 2) return !N;
+int k;
+int i = 0;
 //         increment k until it becomes just greater than N
-        while((k = pow(2,i++)-1 ) < N);
-	return k-N;		    ;
+while ((k = pow(2, i++) - 1) < N)
+	;
+return k - N;
+;
